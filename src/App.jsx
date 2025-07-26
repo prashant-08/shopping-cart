d8cdb8f45b379ce5efef2310c537c05d804aded3
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-07-26 17:13:11 +0530

    Added basic routing react router dom pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
+
+import MainRoutes from './routes/MainRoutes'
 
 function App() {
 
   return (
     <div className='app-wrapper'>
     <Header color="light" light={true} expand="md" container="md"/>     
-    <Home />
+    <MainRoutes />
     <Footer />
     </div>
   )
---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import "./Error.css";
+
+function Error () {
+    return (
+        <div className="error-wrapper">   
+            <img src="https://static.vecteezy.com/system/resources/previews/008/568/878/non_2x/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space-site-crash-on-technical-work-web-design-template-with-chatbot-mascot-cartoon-online-bot-assistance-failure-vector.jpg" alt="error image" />
+        </div>
+    )
+}
+
+export default Error;
