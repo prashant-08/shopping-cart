dddf6c7ff02da4b5585cfcd6d4746c8a0fa9dab4
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-07-26 16:50:21 +0530

    added homepage ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <div className='app-wrapper'>
     <Header color="light" light={true} expand="md" container="md"/>     
-    <div>Some content</div>
+    <Home />
     <Footer />
     </div>
   )
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import CategoryItem from "../../components/CategoryItem/CategoryItem";
+
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h2 className="home-title text-center">Welcome to Shop Cart</h2>
+        <div className="category-list d-flex flex-row justify-content-between align-items-center">
+          <CategoryItem itemName="All Products" />
+        </div>
+        <div className="category-title text-center">
+          Select a category to start shopping
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
