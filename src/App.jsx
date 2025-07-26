76400b82c0b75969ea3b5eff78b5282a58cce4a2
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-07-26 15:56:38 +0530

    added some css

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Footer from './components/Footer/Footer'
 function App() {
 
   return (
-    <>
+    <div className='app-wrapper'>
     <Header color="light" light={true} expand="md" container="md"/>     
     <div>Some content</div>
     <Footer />
-    </>
+    </div>
   )
 }
 
